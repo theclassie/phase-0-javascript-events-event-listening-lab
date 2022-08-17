9e9b5a4693f33c7fbcae55f1e3334f41663f49f4
tristyn <trisclassie@gmaiil.com>
2022-08-17 00:49:55 -0600

    commit tree f8670b11c6e88efa26e8a309f35236822c79c793

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function addingEventListener() {
-    const input = document.getElementById('input');
+  const input = document.getElementById('button');
 
-    function clickAlert() {
-      alert('I was clicked!');
-    }
-    
-    input.addEventListener('click', clickAlert);
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+  
+  input.addEventListener('click', clickAlert);
 }
